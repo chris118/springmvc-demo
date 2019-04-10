e51d9dd6799077e7ae5841646a5240517a4a9614
xiaopeng <12405301@qq.com>
2019-04-10 16:34:50 +0800

    update

---
+++ src/main/java/controllers/TestController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+//    // 返回 index.jsp
+//    @RequestMapping("/index")
+//    public String helloWord(){
+//        System.out.print("access index controller");
+//
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String helloWord(ModelMap model) {
+        model.addAttribute("name", "Chris");
+        return "index";
+    }
+
+//    // 返回 ModelAndView
+//    @RequestMapping(value = "/index", method = RequestMethod.GET)
+//    public ModelAndView helloWord() {
+//        return new ModelAndView("user");
+//    }
+}
